39de9beabf1f357c33fe10f1870afad262268c97
Tanmay Sutar <58669933+Tanny2109@users.noreply.github.com>
2025-01-31 06:30:49 +0000

    basic ai text detection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+# Use a pipeline as a high-level helper
+from transformers import pipeline
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+pipe = pipeline("text-classification", model="yuchuantian/AIGC_detector_env2")
+# st.title("🎈 My new app")
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
+
+with st.form("AI detection"):
+    text = st.text_area("Enter text here...")
+    btn = st.form_submit_button("enter")
+
+if btn: 
+    out = pipe(text)
+    label = out[0]['label']
+    if label == 0:
+        classify='bot generated.'
+    else:
+        classify='human generated.'
+    st.write(classify)
